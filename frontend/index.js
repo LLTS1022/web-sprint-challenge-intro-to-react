81ea9d29dc56ff7b007324e94e138013329268e3
Raishawn Jones <mrjones78945@gmail.com>
2024-04-13 00:38:31 -0500

    first

--- frontend/index.js
+++ frontend/index.js
@@ -10,3 +10,6 @@ const root = createRoot(domNode)
 root.render(
   <App />
 )
+//index.js largely responsible for only rendering the App component.
+//The App component is responsible for rendering the other components.
+//The other components are responsible for rendering their own children.
